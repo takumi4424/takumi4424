7eccf82887fa5f9ce01d7d3bd7de3e6853b100f5
Takumi Kodama <57510056+takumi4424@users.noreply.github.com>
2021-10-18 01:24:28 +0900

    WIP

--- myenv/scripts/setup_ubuntu.sh
+++ myenv/scripts/setup_ubuntu.sh
@@ -4,6 +4,28 @@ set -eu
 here="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 tempdir="$(mktemp -d)"
 
+################################################################################
+################################### ホスト名 ###################################
+################################################################################
+
+################################################################################
+############################## aptパッケージ関連 ###############################
+################################################################################
+# Homebrewでインストールするソフトウェアのリスト
+apt_apps=(
+    fish
+    docker
+    dialog
+    lima
+    jq
+)
+# 準備
+if ! which fish >/dev/null; then sudo apt-add-repository ppa:fish-shell/release-3; fi
+
+
+################################################################################
+################################# フォント関連 #################################
+################################################################################
 if ! [[ -d /usr/share/fonts/HackGen ]]; then
     curl -Lo "$tempdir/HackGen.zip" https://github.com/yuru7/HackGen/releases/download/v2.5.1/HackGen_v2.5.1.zip
     unzip "$tempdir/HackGen.zip"
@@ -11,24 +33,26 @@ if ! [[ -d /usr/share/fonts/HackGen ]]; then
     fc-cache -fv
 fi
 
-if ! which fish >/dev/null; then
-    sudo apt-add-repository ppa:fish-shell/release-3
-    sudo apt-get update
-    sudo apt-get install -y fish
-fi
-if [[ $(cat /etc/passwd | grep -E "^$(id -un):" | cut -d : -f 7) != $(which fish) ]]; then
-    sudo chsh -s "$(which fish)" "$(id -un)"
-fi
-if ! [[ -f ~/.config/fish/functions/fisher.fish ]]; then
-    curl https://raw.githubusercontent.com/jorgebucaran/fisher/HEAD/functions/fisher.fish --create-dirs -Lo ~/.config/fish/functions/fisher.fish
-fi
-# fish -c 'fisher install oh-my-fish/theme-bobthefish'
+################################################################################
+################################### Fish関連 ###################################
+################################################################################
+# if [[ $(cat /etc/passwd | grep -E "^$(id -un):" | cut -d : -f 7) != $(which fish) ]]; then
+#     sudo chsh -s "$(which fish)" "$(id -un)"
+# fi
 
+################################################################################
+########################## その他アプリケーション設定 ##########################
+################################################################################
+# ターミナルプロファイルの設定
+# このUUIDはてきとう
 uuid='9fd90481-da24-477a-955c-6797762f19d4'
 profiles='/org/gnome/terminal/legacy/profiles:'
 if ! dconf list "$profiles/" | grep "$uuid" >/dev/null; then
-    dconf load "$profiles/:$uuid/" < "$here/../gterminal.preferences"
+    # gterminal.preferencesがない場合
+    # gterminal.preferencesをシステムに追加(uuid: $uuid)
+    dconf load "$profiles/:$uuid/" < "$resourcedir/gterminal.preferences"
+    # プロファイルリストに追加
     dconf write "$profiles/list" "$(dconf read $profiles/list | tr "'" '"' | jq -c ". += [\"$uuid\"]" | tr '"' "'")"
+    # デフォルトを{uuid: $uuid}に設定
     dconf write "$profiles/default" "'$uuid'"
 fi
-# echo $list
