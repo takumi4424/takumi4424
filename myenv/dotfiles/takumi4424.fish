7cd0437d8052cd5c920af0dbcbc83cbf65f6e007
Takumi Kodama <57510056+takumi4424@users.noreply.github.com>
2021-10-21 22:00:22 +0900

    mac以外でtakumi4424.fishが死んでいたので修正

--- myenv/dotfiles/takumi4424.fish
+++ myenv/dotfiles/takumi4424.fish
@@ -3,7 +3,7 @@ set -x PATH ~/.takumi4424/myenv/bin $PATH
 if test (uname -s) = 'Darwin'
     alias lima_start_docker='limactl start ~/.takumi4424/myenv/lima/docker.yaml'
     set -x DOCKER_HOST 'ssh://lima-docker'
-else if grep 'NAME="Ubuntu"'
+else if grep 'NAME="Ubuntu"' /etc/os-release >/dev/null
     alias open='xdg-open'
     alias pbcopy='xsel --clipboard --input'
 end
