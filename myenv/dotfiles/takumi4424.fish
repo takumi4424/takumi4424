a20c116a1f410311af6d53b7967fa70982e87a1e
Takumi Kodama <57510056+takumi4424@users.noreply.github.com>
2021-10-22 20:00:50 +0900

    PATH内で重複すること(VSCodeのターミナルなど)があったので修正

--- myenv/dotfiles/takumi4424.fish
+++ myenv/dotfiles/takumi4424.fish
@@ -1,4 +1,8 @@
-set -x PATH ~/.takumi4424/myenv/bin $PATH
+set -x TAKUMI4424_BIN ~/.takumi4424/myenv/bin
+
+if ! echo "$PATH" | grep "$TAKUMI4424_BIN" >/dev/null
+    set -x PATH "$TAKUMI4424_BIN" $PATH
+end
 
 if test (uname -s) = 'Darwin'
     alias lima_start_docker='limactl start ~/.takumi4424/myenv/lima/docker.yaml'
