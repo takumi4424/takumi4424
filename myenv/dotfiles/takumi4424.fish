1313c4118c3e4421063400bdf20aab95c84792d7
Takumi Kodama <57510056+takumi4424@users.noreply.github.com>
2021-10-28 09:02:37 +0900

    code-container補完機能追加

--- myenv/dotfiles/takumi4424.fish
+++ myenv/dotfiles/takumi4424.fish
@@ -1,13 +1,21 @@
+# プログラム置き場
 set -x TAKUMI4424_BIN ~/.takumi4424/myenv/bin
-
-if ! echo "$PATH" | grep "$TAKUMI4424_BIN" >/dev/null
+# パス通す(重複を防ぎつつ)
+if ! echo "$PATH" | grep -F "$TAKUMI4424_BIN" >/dev/null
     set -x PATH "$TAKUMI4424_BIN" $PATH
 end
 
 if test (uname -s) = 'Darwin'
-    alias lima_start_docker='limactl start ~/.takumi4424/myenv/lima/docker.yaml'
-    set -x DOCKER_HOST 'ssh://lima-docker'
+    # MacOS用設定
+    # alias lima_start_docker='limactl start ~/.takumi4424/myenv/lima/docker.yaml'
+    # set -x DOCKER_HOST 'ssh://lima-docker'
 else if grep 'NAME="Ubuntu"' /etc/os-release >/dev/null
+    # Ubuntu用設定
     alias open='xdg-open'
     alias pbcopy='xsel --clipboard --input'
+end
+
+# コマンド補完設定(code-container)
+if which code >/dev/null && which docker >/dev/null
+    complete -c code-container -d 'Container' -x -a '(docker ps -a --format "{{.Names}}")'
 end
